38db981d70885f758f8da309942d63c848ce0a1b
carl-fredrik <carl-fredrik.grimberg@northvolt.com>
2026-04-06 15:41:25 +0200

    add udp pkg

---
+++ udp/udp.go
@@ -0,0 +1,32 @@
+package udp
+
+import (
+	"io"
+	"net"
+)
+
+type Client interface {
+	io.ReadWriteCloser
+}
+
+type Server interface {
+	ReadFrom([]byte) (int, net.Addr, error)
+	WriteTo([]byte, net.Addr) (int, error)
+	Close() error
+}
+
+func NewServer(addr string) (Server, error) {
+	a, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return net.ListenUDP("udp", a)
+}
+
+func NewClient(addr string) (Client, error) {
+	a, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return net.DialUDP("udp", nil, a)
+}
